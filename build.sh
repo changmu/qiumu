1f1764b44d4a931820e4b25efd4f905f43afea9d
qzg <2276479303@qq.com>
2017-05-31 11:42:35 +0800

    update

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
+# usage: ./build.sh -j4 install 
 set -x
 
 SOURCE_DIR=`pwd`
 BUILD_DIR=${BUILD_DIR:-../build}
 BUILD_TYPE=${BUILD_TYPE:-release}
-INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
+# INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install-cpp11}
+INSTALL_DIR=${INSTALL_DIR:-/usr/local}
 BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-1}
 
 mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
